7fa34abee6dede765dd702e92c9ffb82f40549e6
Andre Pardini <Drezilad@gmail.com>
2022-06-03 05:06:30 -0600

    commit on 'main'

--- AWSbasic.tf
+++ AWSbasic.tf
@@ -4,8 +4,37 @@ provider "aws" {
     secret_key = ""
 }
 
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = ""
+}
+
 #tbd
 
-resource "aws_instance" "server-xyz" {
-    
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["1234"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.medium"
+
+  tags = {
+    Name = "7dtdServer"
+  }
+}
 }
